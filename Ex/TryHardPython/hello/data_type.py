30cba4815498538d90a8e349cc651303c0dbe87e
Nosense <vanvux99@gmail.com>
2021-09-25 20:54:35 +0700

    update cuoi ngay

---
+++ Ex/TryHardPython/hello/data_type.py
@@ -0,0 +1,36 @@
+
+print()
+age = 30
+counter = 60
+
+string1 = "freetuts.net"
+string2 = 'cuong'
+
+str = 'Hello World!'
+ 
+# print(str)          # In ra toàn bộ chuỗi
+# print(str[-3])       # In phân tử đầu tiên của chuỗi
+# print(str[2:5])     # In phần tử thứ 3 đến thứ 6, tại vì chỉ mục bắt đầu từ 0
+# print(str[2:])      # In từ vị trí thứ 3 đến cuối chuỗi
+# print(str * 2)      # In chuỗi hai lần
+# print(str + "TEST") # Nối chuỗi
+
+str = 'freetuts.net'
+# str = str.replace(str[3], ' ')
+print(str + '\ooo') #
+
+# for i in str:
+#     print(i)
+
+# Trả về True
+# print('f' in 'freetuts.net')
+ 
+# # Trả về False
+# print('A' not in 'freetuts.net')
+
+thislist = ["apple", "banana", "cherry"]
+# if ("apple" in thislist) == True:
+#   print("Apple có trong danh sách này")
+thislist.clear()
+print(thislist)
+
